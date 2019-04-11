c0b693bc9aad81cb786f3e68e363a23820d6fe31
akiel <akiel@aleph.engineering>
2019-04-10 23:59:19 -0400

    Add first working version

---
+++ main.py
@@ -0,0 +1,117 @@
+import re
+
+
+class PhoneBook:
+    phones = []
+
+    @staticmethod
+    def parse_number(line: str) -> str:
+        """
+        Parses a Bulgarian phone number on any format
+        :param line: text where the supposed number is without any extra data
+        :return: either the phone number or None
+        """
+        number = None
+        if line.startswith('00359'):
+            number = line.replace('00', '+', 1)
+        elif line.startswith('08'):
+            number = line.replace('0', '+359', 1)
+        elif line.startswith('+359'):
+            number = line
+
+        if number and not re.match(r'^\+3598[789][2-9]\d{6}$', number):
+            return None
+
+        return number
+
+    def load_from_file(self, file: str):
+        """
+        Loads name,phone pairs from a file into self.phones
+        :param file: path to the file containing the data
+        :return: Nothing
+        """
+        self.phones = []
+
+        phones_file = open(file, 'r')
+        for line in phones_file:
+            try:
+                name, phone = [part.strip() for part in line.split(',')]
+                self.add_pair(name, phone)
+            except ValueError:
+                continue
+        phones_file.close()
+
+    def add_pair(self, name: str, number: str):
+        """
+        Add a new pair of name, number to the phones array
+        :param name: name of the contact
+        :param number: number of the contact
+        :return: Nothing
+        """
+        phone = self.parse_number(number)
+        if phone:
+            self.phones.append({'name': name, 'phone': phone})
+            # Sort after inserting to avoid sorting when printing
+            self.phones = sorted(self.phones, key=lambda pair: pair['name'])
+        else:
+            raise ValueError("")
+
+    def get_phone_number(self, name: str) -> str:
+        """
+        Gets a specific phone number by name
+        :param name: name of the contact
+        :return: either the phone number corresponding to this name or None
+        """
+        for number in self.phones:
+            if number['name'] == name:
+                return number['phone']
+        return None
+
+    def delete_pair(self, name: str):
+        """
+        Deletes the pair name, number from the phones array
+        :param name: name of the contact
+        :return: Nothing
+        """
+        for number in self.phones:
+            if number['name'] == name:
+                self.phones.remove(number)
+
+    def print_all_numbers(self):
+        """
+        Prints all numbers which are already sorted by name
+        :return:
+        """
+        for number in self.phones:
+            print(f'Name: {number["name"]} Number:{number["phone"]}')
+
+    def set_outgoing_calls(self, name, outgoing_calls: int) -> bool:
+        """
+        Set the amount of outgoing calls for a number
+        :param name: name of the contact
+        :param outgoing_calls: amount of outgoing calls
+        :return:
+        """
+        for number in self.phones:
+            if number['name'] == name:
+                number['outgoing calls'] = outgoing_calls
+                return True
+        return False
+
+    def print_phones_sorted_by_outgoing_calls(self, limit=5):
+        """
+        Print the phones sorted by outgoing calls
+        :param limit: how many numbers to show in the output
+        :return:
+        """
+        for i, number in enumerate(sorted(self.phones,
+                                          key=lambda pair: pair['outgoing calls'] if 'outgoing calls' in pair else 0,
+                                          reverse=True)):
+            if i == limit:
+                break
+
+            outgoing_calls = 0
+            if 'outgoing calls' in number:
+                outgoing_calls = number["outgoing calls"]
+
+            print(f'Name: {number["name"]} Number:{number["phone"]} Outgoing calls: {outgoing_calls}')
---
+++ main_tests.py
@@ -0,0 +1,80 @@
+import unittest
+
+from main import PhoneBook
+
+NUMBERS_DO_NOT_MATCH = 'Number do not match'
+RESULT_IS_NOT_NONE = 'Result is not None'
+DELETE_DID_NOT_WORK = 'Delete did not work'
+
+
+class PhoneBookTestCase(unittest.TestCase):
+    def setUp(self):
+        self.phonebook = PhoneBook()
+
+    def test_load_from_file(self):
+        self.phonebook.load_from_file('phonebook.txt')
+        self.assertEqual([{'name': 'akiel', 'phone': '+359878123456'}, {'name': 'monik', 'phone': '+359878123459'}],
+                         self.phonebook.phones, 'Phonebook loading failed')
+
+    def test_parse_function(self):
+        self.assertEqual('+359878123456', self.phonebook.parse_number('+359878123456'), NUMBERS_DO_NOT_MATCH)
+
+    def test_parse_function_with_two_zero(self):
+        self.assertEqual('+359878123456', self.phonebook.parse_number('00359878123456'), NUMBERS_DO_NOT_MATCH)
+
+    def test_parse_function_with_one_zero(self):
+        self.assertEqual('+359878123456', self.phonebook.parse_number('0878123456'), NUMBERS_DO_NOT_MATCH)
+
+    def test_parse_function_cant_parse_without_plus(self):
+        self.assertIsNone(self.phonebook.parse_number('359878123456'), RESULT_IS_NOT_NONE)
+
+    def test_parse_function_invalid_operator_code(self):
+        self.assertIsNone(self.phonebook.parse_number('+359008123456'), RESULT_IS_NOT_NONE)
+
+    def test_parse_function_wrong_digit(self):
+        self.assertIsNone(self.phonebook.parse_number('+359870123456'), RESULT_IS_NOT_NONE)
+
+    def test_parse_function_with_random_data(self):
+        self.assertIsNone(self.phonebook.parse_number('random text'), RESULT_IS_NOT_NONE)
+        self.assertIsNone(self.phonebook.parse_number('+359 random text'), RESULT_IS_NOT_NONE)
+        self.assertIsNone(self.phonebook.parse_number('+3590random text'), RESULT_IS_NOT_NONE)
+        self.assertIsNone(self.phonebook.parse_number('+3598random text'), RESULT_IS_NOT_NONE)
+
+    def test_delete_pair(self):
+        self.phonebook.phones = [{'name': 'akiel', 'phone': '+359878123456'},
+                                 {'name': 'monik', 'phone': '+359878123459'}]
+        self.phonebook.delete_pair('akiel')
+        self.assertEqual([{'name': 'monik', 'phone': '+359878123459'}], self.phonebook.phones, DELETE_DID_NOT_WORK)
+
+    def test_get_phone_number(self):
+        self.phonebook.phones = [{'name': 'akiel', 'phone': '+359878123456'},
+                                 {'name': 'monik', 'phone': '+359878123459'}]
+        self.assertEqual('+359878123459', self.phonebook.get_phone_number('monik'))
+
+    def test_get_phone_number_returns_none_if_not_found(self):
+        self.phonebook.phones = [{'name': 'akiel', 'phone': '+359878123456'},
+                                 {'name': 'monik', 'phone': '+359878123459'}]
+        self.assertIsNone(self.phonebook.get_phone_number('nobody'), RESULT_IS_NOT_NONE)
+
+    def test_print_numbers(self):
+        self.phonebook.add_pair('b', '+359878123459')
+        self.phonebook.add_pair('a', '+359878123458')
+        self.phonebook.add_pair('c', '+359878123457')
+        self.phonebook.print_all_numbers()
+
+    def test_set_outgoing_calls(self):
+        self.phonebook.phones = [{'name': 'akiel', 'phone': '+359878123456'}, ]
+        self.assertTrue(self.phonebook.set_outgoing_calls('akiel', 5), 'Could not find this contact')
+        self.assertEqual([{'name': 'akiel', 'phone': '+359878123456', 'outgoing calls': 5}, ], self.phonebook.phones,
+                         NUMBERS_DO_NOT_MATCH)
+        self.assertFalse(self.phonebook.set_outgoing_calls('nobody', 5))
+
+    def test_print_phones_sorted_by_outgoing_calls(self):
+        self.phonebook.phones = [{'name': 'a', 'phone': '+359878123456', 'outgoing calls': 1},
+                                 {'name': 'b', 'phone': '+359878123459', 'outgoing calls': 3},
+                                 {'name': 'c', 'phone': '+359878123456', 'outgoing calls': 7},
+                                 {'name': 'd', 'phone': '+359878123456', 'outgoing calls': 5},
+                                 {'name': 'e', 'phone': '+359878123456', 'outgoing calls': 1},
+                                 {'name': 'f', 'phone': '+359878123456'},
+                                 ]
+        self.phonebook.print_phones_sorted_by_outgoing_calls(5)
